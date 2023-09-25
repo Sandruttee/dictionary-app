4e745c9d993df3ed047864725571967431da8071
Sandruttee <sandra.placenyte@gmail.com>
2023-09-25 16:17:12 +0200

    Set up a basic project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+function App() {
+  return (
+    <div className="App ">
+      <div className="container">
+        <div className="title">DICTIONARY</div>
+        <main>
+          {" "}
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Sandra Pačenytė</footer>
+      </div>{" "}
+    </div>
+  );
+}
+
+export default App;
