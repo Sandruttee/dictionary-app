3ae92b868bf06d0905a92a06ee47049a766c68cf
Sandruttee <sandra.placenyte@gmail.com>
2023-09-25 20:34:50 +0200

    got API response

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -29,6 +32,7 @@ export default function Dictionary() {
           placeholder="type a word that's on your mind..."
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
