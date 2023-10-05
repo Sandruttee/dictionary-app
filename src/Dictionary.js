e426d05a4f62b743eb2d97d8c92400187412c459
Sandruttee <sandra.placenyte@gmail.com>
2023-10-05 21:25:39 +0200

    Added an api call from shecodes for pictures

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleSheCodesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -17,6 +23,13 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    let sheCodesApiKey = "1be70t3b32a048fab4oc2947a8669607";
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${sheCodesApiKey}`;
+    axios
+      .get(sheCodesApiUrl, {
+        headers: { Authorization: `Bearer${sheCodesApiKey}` },
+      })
+      .then(handleSheCodesResponse);
   }
 
   function handleKeywordChange(event) {
@@ -32,6 +45,7 @@ export default function Dictionary() {
         />
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
